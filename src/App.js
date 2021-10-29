44537b69c74c9b3e8f88346d295b5cac9b7b38e4
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-10-29 19:06:24 +0100

    create the main page and import material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import Home from "./pages/home/Home";
 function App() {
-  return <div className="App"></div>;
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+import "./home.css";
+
+export default function Home() {
+  return <>Home page</>;
+}
